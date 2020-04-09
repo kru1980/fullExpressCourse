d9c7791cd585ff944e3166a8b8da689e1dea1970
kru1980 <kru1980@bk.ru>
2020-04-09 20:01:46 +0500

    @handlebars/allow-prototype-access

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const PORT = process.env.PORT || 3000;
 const path = require("path");
 const mongoose = require("mongoose");
 const User = require("./models/user");
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
 
 const app = express();
+
 const hbs = exphbs.create({
   defaultLayout: "main",
-  extname: "hbs"
+  extname: "hbs",
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
 
 
 });
