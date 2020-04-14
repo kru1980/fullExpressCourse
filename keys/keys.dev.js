d2d2efc91aa5d0c9e51968984f1cc29962f66d8f
kru1980 <kru1980@bk.ru>
2020-04-14 12:18:36 +0500

    add keys

---
+++ keys/keys.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB_URI: "mongodb://localhost/shop",
+  SESSION_SECRET: "some secret value",
+  SENDGRID_API_KEY:
+    "",
+  EMAIL_FROM: "nodejs@email.ru",
+  BASE_URL: "http://localhost:3000",
+};
