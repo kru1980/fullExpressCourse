fc1c3b73744376844d4f58a684d58162c0856cf7
kru1980 <kru1980@bk.ru>
2020-04-14 11:58:21 +0500

    new keys

---
+++ keys/keys.dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGODB_URI: "mongodb+srv://kru1980:022749@cluster0-ezfph.mongodb.net/shop",
+    SESSION_SECRET: "keyboard cat",
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'nodejs@email.ru',
+    BASE_URL: 'http://localhost:3000'
+  };
+  
